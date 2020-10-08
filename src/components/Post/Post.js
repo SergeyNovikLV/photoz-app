2bf17e5554ced93e31c9e119b4d78cf3c841d734
SergyNovikLV <mr.noviks@gmail.com>
2020-10-08 13:17:20 +0300

    Finished post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import { Card } from 'reactstrap'
+import Image from 'components/Image'
+import Comments from 'components/Comments'
+import './post.scss'
 
-function Post() {
-  return <h3>Post</h3>
+function Post({ title, imgUrl, postId }) {
+  return (
+    <Card className="mt-4">
+      <h4 className="post-title">{title}</h4>
+      <Image alt="butterfly" className="post-image" src={imgUrl} />
+      <Comments postId={postId} />
+    </Card>
+  )
 }
 export default Post
